8c5af66946d9337ba25481c8703e656f1c98ac96
AmrAlaaYassen <amralaa566@gmail.com>
2018-06-21 20:59:16 +0200

    track moves & Store them

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,7 +5,7 @@ import { NavController, Platform, AlertController } from 'ionic-angular';
 import { Subscription } from 'rxjs/Subscription';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Storage } from '@ionic/storage';
-
+import {filter} from 'rxjs/operators';
 
 declare var google;
 
@@ -60,8 +60,53 @@ export class HomePage {
 
      });
   }
-  showMap () {
 
+  startTracking () {
+    this.isTracking = true;
+    this.trackedRoute = [] ;
+
+    this.positionSubscription= this.geolocation.watchPosition().pipe(filter(p=>p.coords !== undefined))
+    .subscribe(data => {
+
+      setTimeout(()=>{
+        this.trackedRoute.push({lat:data.coords.latitude , lng:data.coords.longitude});
+        this.redrawPath(this.trackedRoute);
+      });
+
+    });
+  }
+
+  redrawPath(path) {
+      if(this.currentMapTrack){
+        this.currentMapTrack.setMap(null);
+      }
+
+      if(path.length>1) {
+        this.currentMapTrack = new google.maps.Polyline( {
+          path:path,
+          geodesic:true,
+          strokeColor:'#ff00ff',
+          strokeOpacity:1.0,
+          strokeWeight:3
+        });
+
+        this.currentMapTrack.setMap(this.map);
+      }
+  }
+
+  stopTracking(){
+    let newRoute = {finished : new Date().getTime , path:this.trackedRoute};
+    this.perviousTricks.push(newRoute);
+    this.storage.set('routes',this.perviousTricks);
+
+    this.isTracking = false ;
+    this.positionSubscription.unsubscribe();
+    this.currentMapTrack.setMap(null);
+  }
+
+
+  showHistoryRoute(route) {
+    this.redrawPath(route);
   }
 
 }
